119804f5e87f5260443e6f0d099b43c0e62acdb3
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-10-12 19:06:52 +0700

    merge seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,6 @@ class UserSeeder extends Seeder
         $user->email = "poomffi@hotmail.com";
         $user->password = bcrypt('12345678');
         $user->role = "admin";
-<<<<<<< HEAD
         $user->save();
 
         $user = new User();
@@ -26,8 +25,6 @@ class UserSeeder extends Seeder
         $user->email = "poomffi@gmail.com";
         $user->password = bcrypt('12345678');
         $user->role = "user";
-=======
->>>>>>> feature/multirole
         $user->save();
     }
 }
