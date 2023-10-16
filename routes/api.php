66eec089dc76ec46ecc1a27fedb1a083a1fdbafe
kornavit <kornavit.p@ku.th>
2023-10-16 16:54:01 +0700

    linebot

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\RepairController;
+use App\Http\Controllers\Api\LinebotController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::post('/line', [LinebotController::class, 'index']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
